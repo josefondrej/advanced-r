d4560b4a72bb790ade75ebb6b18954e815431576
Josef Ondrej <josef.ondrej@outlook.com>
2020-12-26 21:01:17 +0100

    Add chap. 4

---
+++ ch_4_subsetting.R
@@ -0,0 +1,28 @@
+# Title     : Subsetting
+# Created by: Josef Ondrej (www.josefondrej.com)
+# Created on: 26.12.20
+
+x = c(2, 4, 3, 5)
+x[c(3, 1)]
+x[order(x)]
+x[c(1, 1)]
+
+x[-c(3, 1)]
+
+x[c(TRUE, FALSE, TRUE, FALSE)]
+
+x[]
+
+x[0]
+
+y = setNames(x, c("a", "b", "c", "d"))
+y
+y["a"]
+# WARNING: Factors are treated as integers when used for subsetting
+
+df = data.frame(a = 1:5, b = 3:7, c = letters[5:9])
+rownames(df)
+df[c("1", "2"), c("b", "c")]
+df[-1:-3, "a", drop = FALSE] # Do not reduce dimension
+x = 1:5
+

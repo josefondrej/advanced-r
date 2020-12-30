d7f365b4f30f134e0b79e6e234b634d346ca10df
Josef Ondrej <josef.ondrej@outlook.com>
2020-12-30 09:56:59 +0100

    Add chap. 19

---
+++ ch_19_quasiquotation.R
@@ -0,0 +1,13 @@
+# Title     : Quasiquotation
+# Created by: Josef Ondrej (www.josefondrej.com)
+# Created on: 28.12.20
+
+library(rlang)
+library(purrr)
+
+# Evaluated vs Quoted arguments
+library(MASS)
+MASS
+foo = "foo"
+
+
